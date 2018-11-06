9bab15f570819e351acec7fda1624415769f4c87
David Wolf <dpwolf@mac.com>
2018-11-06 11:29:27 -0500

    update prepare_images to point to lib directory & package-json

--- bin/prepare_images.sh
+++ bin/prepare_images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-find ./node_modules/antd-mobile-rn/es -iname "*@2x.png" | \
+find ./node_modules/antd-mobile-rn/lib -iname "*@2x.png" | \
     sed "p;s/@2x.png$/.png/" | \
     xargs -n2 cp

67712707e7cb4ed1d76ed466ef3729a5ed28b65b
Kevan Davis <kevan.davis@handshake.com>
2018-12-05 14:32:00 -0600

    sketch improvements

--- bin/sketch.sh
+++ bin/sketch.sh
@@ -1,19 +1,45 @@
 #!/usr/bin/env bash
 
+sketchtool=/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool
+plugins="$($sketchtool show plugins)"
+
+sketch () {
+    plugin=$1
+    command=$2
+    paramName=$3
+    paramValue=$4
+
+    if [ -n $paramName ]; then
+        $sketchtool run "$plugins/$plugin.sketchplugin" $command \
+            --context="{\"$paramName\":\"$paramValue\"}"
+    else
+        $sketchtool run "$plugins/$plugin.sketchplugin" $command
+    fi
+}
+
+import2sketch () {
+    sketch page_utils setOrAddPage target "$(node -p "require('./$1.asketch.json').name")"
+    cat ./$1.asketch.json | pbcopy
+    sketch asketch2sketch paste
+}
+
 # Install the Almost Sketch 2 Sketch plugin if it's not already installed:
-ls ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/asketch2sketch.sketchplugin || \
+ls "$plugins/asketch2sketch.sketchplugin" 2>/dev/null > /dev/null || \
     open ./storybook/asketch2sketch.sketchplugin
+
+# Install the Page Utils plugin if it's not already installed:
+ls "$plugins/page_utils.sketchplugin" 2>/dev/null > /dev/null || \
+    open ./storybook/page_utils.sketchplugin
 
 # Run the Story2Sketch export:
 story2sketch
 
-# Fix some minor issues in the export:
-./bin/tweak_sketch_export.js
+# Split the export into multiple pages:
+./bin/sketch_postprocess.js
 
-# Copy export to clipboard
-cat ./stories.asketch.json | pbcopy
-
-# Import into sketch
-/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool run \
-    ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/asketch2sketch.sketchplugin \
-    paste
+for file in ./*.asketch.json; do
+    if [[ $file != "./stories.asketch.json" ]]; then
+        import2sketch $(echo $file | cut -d "/" -f2 | cut -d "." -f1)
+        rm $file
+    fi
+done

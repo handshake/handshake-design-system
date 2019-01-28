4bcc277b19ae95e5cbdf1cc584305db6a0b3af42
Kevan Davis <kevan.davis@handshake.com>
2019-01-28 10:55:57 -0600

    derp. spin is from antd not antd-mobile

--- src/_components/spin/index.web.js
+++ src/_components/spin/index.web.js
@@ -6,5 +6,5 @@
 //       this component works may change slightly.
 //       YOU HAVE BEEN WARNED.
 
-export { default } from "antd-mobile/lib/spin";
-import "antd-mobile/lib/spin/style";
+export { default } from "antd/lib/spin";
+import "antd/lib/spin/style";
